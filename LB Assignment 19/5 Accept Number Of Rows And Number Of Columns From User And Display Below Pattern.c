a40427f9e7d89e36e75f67c80352a2eaa1fd4fa7
AnkitaS1996 <Ankitashedage2661@gmail.com>
2021-02-23 13:14:28 +0530

    Assignment Program 19.5

---
+++ LB Assignment 19/5 Accept Number Of Rows And Number Of Columns From User And Display Below Pattern.c
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Statement 19.5 = Accept Number Of Rows And Number Of Columns From User And Display Below Pattern.c
+//      Input =  iRow = 4    iCol = 4
+//     Output =  1  2  3   4
+//                  2  3   4
+//                     3   4
+///                        4
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<conio.h>
+void Pattern(int,int);
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    printf("\n========================================================================\n");
+    printf("\n Enter Number Of Rows And Number Of Columns : ");
+    scanf("%d%d",&iValue1,&iValue2);
+    printf("\n========================================================================\n");
+    Pattern(iValue1,iValue2);
+    printf("\n========================================================================\n");
+    printf("\n Thank You...!!! \n\n Press Any Key to continue..!!\n");
+    printf("\n========================================================================\n");
+    getch();
+    return 0;
+}
+void Pattern(int iRow,int iCol)
+{
+    int No1 = 0, No2 = 0,Num = 1;
+    printf("\n========================================================================\n\n");
+    for(No1 = 1;No1 <= iRow;No1++)
+    {
+        Num = 1;
+        for(No2 = 1;No2 <= iCol;No2++)
+        {
+            if(No1 == No2 || No1 <= No2)
+            {
+                printf(" %3d",Num);
+            }
+            else
+            {
+                printf("    ");
+            }
+            Num++;
+        }
+        printf("\n");
+    }
+    printf("\n========================================================================\n");
+    return;
+}
