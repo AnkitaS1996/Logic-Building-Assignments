81576c225d1c06fbfb85980a4bab0d5eea4a4da9
AnkitaS1996 <Ankitashedage2661@gmail.com>
2021-02-24 23:40:54 +0530

    Assignment Program 20.2

---
+++ LB Assignment 20/20.2 Accept Number Of Rows And Number Of Columns From User And Display Below Pattern.c
@@ -0,0 +1,52 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////// Statement 20.2 = Accept Number Of Rows And Number Of Columns From User And Display Below Pattern.
+//////      Input : iRow = 4 iCol = 4
+//////     Output : * * * #
+//                  * * # @
+//                  * # @ @
+//                  # @ @ @
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<conio.h>
+void Pattern(int,int);
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    printf("\n========================================================================\n");
+    printf("\n Enter Number Of Rows And Number Of Columns : ");
+    scanf("%d%d",&iValue1,&iValue2);
+    printf("\n========================================================================\n");
+    Pattern(iValue1,iValue2);
+    printf("\n========================================================================\n");
+    printf("\n Thank You...!!! \n\n Press Any Key to continue..!!\n");
+    printf("\n========================================================================\n");
+    getch();
+    return 0;
+}
+void Pattern(int iRow,int iCol)
+{
+    int No1 = 0, No2 = 0;
+    printf("\n========================================================================\n\n");
+    for(No1 = 1;No1 <= iRow;No1++)
+    {
+        for(No2 = 1;No2 <= iCol;No2++)
+        {
+            if(No1 + No2 == iCol+1)
+            {
+                printf(" %c",'#');
+            }
+            else if(No1+No2 > iCol+1)
+            {
+                printf(" %c",'@');
+            }
+            else
+            {
+                printf(" %c",'*');
+            }
+        }
+        printf("\n");
+    }
+    printf("\n========================================================================\n");
+    return;
+}
